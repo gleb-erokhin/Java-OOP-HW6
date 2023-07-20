98178bd844a2258aab56ecddd1489ae484af1f99
gleb-erokhin <gleb.erokhin@gmail.com>
2023-07-20 09:16:16 +0300

    добавление файлов 4 задания в проект

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class Calculator {
+
+    public double sum(List<? extends Number> numbers) {
+        double res = 0.0;
+        for (Number n : numbers) {
+            res += n.doubleValue();
+        }
+        return res;
+    }
+
+    public double multiplication(List<? extends Number> numbers) {
+        double res = 1.0;
+        for (Number n : numbers) {
+            res *= n.doubleValue();
+        }
+        return res;
+    }
+
+    public double devision(List<? extends Number> numbers) {
+        double res = numbers.get(0).doubleValue();
+        for (int i = 1; i < numbers.size(); i++) {
+            res = res / numbers.get(i).doubleValue();
+        }
+        return res;
+    }
+
+}
