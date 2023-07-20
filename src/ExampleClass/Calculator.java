a787e45cb4bcb4ce125f5b7b3ca98a820d9a5c01
gleb-erokhin <gleb.erokhin@gmail.com>
2023-07-20 16:50:44 +0300

    Изменение название пакета эталона исходной задачи

--- src/ExampleClass/Calculator.java
+++ src/ExampleClass/Calculator.java
@@ -1,4 +1,4 @@
-package oldClass;
+package ExampleClass;
 
 import java.util.List;
 
--- src/ExampleClass/Main.java
+++ src/ExampleClass/Main.java
@@ -1,4 +1,4 @@
-package oldClass;
+package ExampleClass;
 
 import java.util.ArrayList;
 import java.util.Arrays;
